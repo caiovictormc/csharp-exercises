70d9d7094f7ed2e7c760068b40605d5d43984edf
caiovictormc <caiovictor31@live.com>
2018-10-07 16:06:43 -0300

    Update exercises

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class Statement
 {
@@ -11,17 +13,19 @@ public class Statement
 
     public bool isQuestion()
     {
-        return this.statement[this.statement.Length - 1] == '?';
+        string cleanString = this.statement.Replace(" ", "");
+        return cleanString[cleanString.Length - 1] == '?';
     }
 
     public bool isNothing()
     {
-        return (this.statement.Length == 0);
+        return string.IsNullOrEmpty(this.statement) || string.IsNullOrWhiteSpace(this.statement);
     }
 
     public bool isYell()
     {
-        return (this.statement.ToUpper() == this.statement);
+        bool letterCheck = this.statement.Any(x => char.IsLetter(x));
+        return (letterCheck && this.statement.ToUpper() == this.statement);
     }
 }
 
@@ -32,28 +36,26 @@ public static class Bob
         Statement declaration = new Statement(statement);
         string response = "Whatever.";
 
-        if (declaration.isYell())
-        {
-            if (declaration.isQuestion())
-            {
-                response = "Calm down, I know what I'm doing!";
-                return response;
-            }
-
-            response = "Whoa, chill out!";
-            return response;
-        }
-
-        if (declaration.isQuestion())
-        {
-            response = "Sure.";
-            return response;
-        }
-
         if (declaration.isNothing())
+            {
+                response = "Fine. Be that way!";
+            }
+        else
         {
-            response = "Fine. Be that way!";
-            return response;
+            if (declaration.isYell())
+            {
+                response = "Whoa, chill out!";
+                if (declaration.isQuestion())
+                {
+                    response = "Calm down, I know what I'm doing!";
+                }
+
+            } else {
+                if (declaration.isQuestion())
+                {
+                    response = "Sure.";
+                }
+            }
         }
 
         return response;
